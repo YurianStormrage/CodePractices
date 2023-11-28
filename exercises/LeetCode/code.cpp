c52b0409ac0c64f56b61e919be48d028e57cfaa7
YurianStormrage <pqqqan@foxmail.com>
2023-11-28 08:27:02 +0800

    add template file code.cpp which can be copied directly into new directory

---
+++ exercises/LeetCode/code.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    struct Test : Solution {
+        vector<int> nums;
+        int target;
+        void run() {
+            auto ans = ;
+            cout << ans << endl;
+            // for_each(ans.begin(), ans.end(),
+            //          [](decltype(ans)::value_type const& x) {
+            //             cout << x << ' ';
+            //          });
+            // cout << endl;
+        }
+    };
+
+    Test t1 {
+        .nums = {},
+        .target = 
+    };
+    Test t2 {
+        .nums = {},
+        .target = 
+    };
+    Test t3 {
+        .nums = {},
+        .target = 
+    };
+
+    t1.run();
+    t2.run();
+    t3.run();
+    
+    return 0;
+}
